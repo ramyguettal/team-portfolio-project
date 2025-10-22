a6acb4bd6a5a7cf24bed8d48529a7fe5871eca8f
unknown <nasrellah.kharroubi@ensia.edu.dz>
2025-10-22 19:52:18 +0100

    feat : add personal portfolio component (Nasro)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// import Nasro from './Nasro/Nasro';
+import Imad from './imad/imad';
+import Nasro from './Nasro/Nasro';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Main App</h2>
+      <Nasro />
     </div>
   );
 }
+
 
 export default App;
